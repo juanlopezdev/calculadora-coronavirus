9d53cd127c1df900b71c9c16616c50f160c57ddf
Juan López <juanlopez.developer@gmail.com>
2020-03-21 01:48:57 -0500

    Clean pages

--- src/components/layout/Footer.vue
+++
@@ -1,7 +0,0 @@
-<template lang="pug">
-  footer.footer.bg-dark.text-white.p-4
-    .container.text-center
-        p.mb-0 
-          | Development by 
-          a.text-reset(href="https://twitter.com/juanlopezdev", target="_blank") @juanlopezdev
-</template>
--- src/components/layout/Header.vue
+++
@@ -1,6 +0,0 @@
-<template lang="pug">
-  nav.navbar.navbar-light.bg-light
-    a.navbar-brand(href="#")
-      img.d-inline-block.align-top(src="../../assets/img/house.png", width="30", alt="")
-      | #YoMeQuedoEnCasa
-</template>
