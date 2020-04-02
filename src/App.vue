b1800846d590a9edf27350cacec12fde79bb2231
Juan López <juanlopez.developer@gmail.com>
2020-04-01 20:17:49 -0500

    Updated infected people to 01/04/2020

--- src/App.vue
+++ src/App.vue
@@ -107,7 +107,7 @@
                 | (*) Cantidad de casos confirmados obtenido del twitter oficial del 
                 a(href="https://twitter.com/Minsa_Peru", target="_blank") Ministerio de Salud del Perú
                 br
-                | (Actualizado al 31/03/2020 21:26:00)
+                | (Actualizado al 01/04/2020 20:16:00)
         // @end Cases
       // @end Main
     // @start Footer
