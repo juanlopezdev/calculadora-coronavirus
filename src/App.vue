1ced190462725d520d7419e2fd67c902088905a3
Juan López <juanlopez.developer@gmail.com>
2020-05-03 22:54:47 -0500

    Infected people to 03/05/2020 and Google Analytics

--- src/App.vue
+++ src/App.vue
@@ -6,7 +6,7 @@
       // @end Header
       // @start Main
       main
-        small.px-3.py-1.d-block.text-right Actualizado al 19/04/2020 23:43:00
+        small.px-3.py-1.d-block.text-right Actualizado al 03/05/2020 22:53:00
         // @start Calculator
         section.section-calculator.py-5
           .container
